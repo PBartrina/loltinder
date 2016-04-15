b471bc3cb0c6202522f2783d89e988540e9aed5f
Pau Martinez <pau.martinez@gft.com>
2016-04-15 12:10:05 +0200

    refine filters

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
   * 5. GET FACEBOOK NUMERIC ID FROM: http://findmyfacebookid.com/
   * 6. GET ACCESS TOKEN FROM THE FOLLOWING URL. IT REDIRECTS TO ANOTHER PAGE QUICKLY, SO GRAB THE URL UP TOP BEFORE IT CHANGES QUICKLY.
   *
-  * https://www.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token 
+  * https://www.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token
   *
   * 7. To run, type 'node app.js FACEBOOK_ID THE_URL_YOU_COPIED_AND_PASTED'
   *
@@ -16,7 +16,8 @@
 
 var tinderbot = require('tinderbot');
 var bot = new tinderbot();
-var _ = require('underscore')
+var _ = require('underscore');
+var s = require("underscore.string");
 
 // Process URL and grab token
 var url = process.argv[3];
@@ -27,47 +28,65 @@ url = url[1].split('&');
 bot.access_token = url[0];
 bot.facebook_id = process.argv[2];
 
-console.log('###########################');
-console.log('Token: ' + bot.access_token);
-console.log('FB ID: ' + bot.facebook_id);
-console.log('CURL: curl -X POST --data "facebook_token='+bot.access_token+'&facebook_id='+bot.facebook_id+'" https://api.gotinder.com/auth');
-console.log('###########################');
+// console.log('###########################');
+// console.log('Token: ' + bot.access_token);
+// console.log('FB ID: ' + bot.facebook_id);
+// console.log('CURL: curl -X POST --data "facebook_token=' + bot.access_token + '&facebook_id=' + bot.facebook_id + '" https://api.gotinder.com/auth');
+// console.log('###########################');
+
+console.log("** CoMe oN LiKe MaChInE!**");
+
 
 var count = 0;
 var interval;
 
-bot.client.authorize(bot.access_token, bot.facebook_id, function(){
-  interval = setInterval(function() {
-    if (!bot.client.isAuthorized) {
-      console.log('Token is invalid. Please get another.');
-      clearInterval(interval);
-    }
+var xaccess = "";
 
-    bot.client.getRecommendations(10, function(error, data){
-      if (data && data.message === "recs timeout") {
-        console.log('Recommendations timeout');
-        return;
-      }
+bot.client.authorize(bot.access_token, bot.facebook_id, function() {
+    bot.client.updatePosition("41.4239352", "2.1660459", function() {
 
-      _.chain(data.results)
-      .each(function(v,k) {
-        console.log(v.name);
-        bot.client.like(v._id, function(error, data) {
-          count++;
-          console.log('Count: ' + count);
-          if (data && data.matched) {
-            console.log('MATCHED!');
-            console.log(user);
-          }
-        //   if (data.matched) {
-        //     bot.client.sendMessage(
-        //       id,
-        //       "You're gorgeous. Let's adventure"
-        //     );
-        //   }
-        });
-      });
+
+        interval = setInterval(function() {
+            if (!bot.client.isAuthorized()) {
+                console.log('Token is invalid. Please get another.');
+                clearInterval(interval);
+            }
+
+            bot.client.getRecommendations(10, function(error, data) {
+                if (data && data.message === "recs timeout") {
+                    console.log('Recommendations timeout');
+
+                    clearInterval(interval);
+                    return;
+                }
+
+                _.chain(data.results)
+                    .each(function(v, k) {
+                        var friends = (v.common_friend_count) ? v.common_friend_count : "0";
+                        var bio = v.bio.toLowerCase();
+
+                        if (s.include(bio, "out of likes")) {
+                            console.log("tas pasao de likes, tete");
+                            clearInterval(interval);
+                        } else if (v.common_friend_count || s.include(bio, "trans")) {
+                            bot.client.pass(v._id, function(error, data) {
+                                if (s.include(bio, "trans")) console.log(v.name + " it's a TRAP!");
+                                if (v.common_friend_count) {
+                                    console.log(v.name, "is friend of: ", v.common_friends);
+                                }
+                            });
+                        } else {
+                            bot.client.like(v._id, function(error, data) {
+                                count++;
+                                console.log('(' + count + ')', v.name);
+                                if (data && data.matched) {
+                                    console.log('MATCHED!', user);
+                                }
+                            });
+                        }
+                    });
+            });
+        }, 5000);
     });
-  }, 5000);
-}); 
+});
 
